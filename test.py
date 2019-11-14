17c4d4ea902db5fa5c1ae19e1ee4e301e128297d
chucklapress <charleslapress@gmail.com>
2019-11-13 22:28:14 -0500

    added files

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+import app
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        app.app.testing = True
+        self.app = app.app.test_client()
+
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_hello(self):
+        rv = self.app.get('/hello/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+    unittest.main()
