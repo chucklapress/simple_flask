2c878cbac1c98e807088e57b4c066fb283c8b57d
chucklapress <charleslapress@gmail.com>
2020-01-09 14:51:43 -0500

    removed comments

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')  # this route is not working
+@app.route('/hello')  
 @app.route('/hello/')
 def hello_world():
     return 'Hello World!\n'
 
-@app.route('/hello/<username>') # dynamic route
+@app.route('/hello/<username>')
 def hello_user(username):
-    # show the user profile for that user
+
     return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')  # open for everyone
+    app.run(host='0.0.0.0')
